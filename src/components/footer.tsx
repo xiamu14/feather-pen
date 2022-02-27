fd633eac2fb2f9d0a96c66e10f57951072dbd80f
ben <x_z_gfree@163.com>
2022-02-27 09:44:55 +0800

    feature change name.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,5 @@
 import style from "./footer.module.css";
 
 export default function Footer() {
-  return <footer className={style["footer"]}>Play Game©2022</footer>;
+  return <footer className={style["footer"]}>Feather Pen©2022</footer>;
 }
